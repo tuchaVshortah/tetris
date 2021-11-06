4c8aaa7ce79aa7590939e8074667f7c4752a0f51
tuchaVshortah <71591558+tuchaVshortah@users.noreply.github.com>
2021-11-06 09:55:45 +0000

    Add files via upload

--- include/tetris.hpp
+++ include/tetris.hpp
@@ -5,21 +5,20 @@
 #include "types.hpp"
 extern const int HEIGHT, WIDTH;
 extern int blockShape, blockSprite, offset;
-extern int inactiveBlockSprites[20][10];
+extern int inactiveBlockSprites[20][10], inactiveBlockSprites2[20][10];
 extern const int allBlockShapes[7][4];
-extern double timer, delay;
-extern bool generateNewFigure;
-extern sf::Text scoreText, score;
+extern double timer, timer2, delay;
+extern bool generateNewFigure, generateNewFigure2, gameOver;
+extern sf::Text scoreText, scoreText2, score, score2, gameOverText;
 extern sf::Sprite sprites[8];
-extern struct Block blocks[4], backupBlocks[4];
+extern struct Block blocks[4], blocks2[4], backupBlocks[4], backupBlocks2[4];
 extern void tetrisInit();
-extern void checkLine(int inactiveBlockSprites[][10], sf::Text &score);
-extern bool checkIfInFrame(Block blocks[]);
+void checkLine(int inactiveBlockSprites[][10], sf::Text &score, int inactiveBlockSprites2[][10], sf::Text &score2);
 extern void saveBlocks(Block backupBlocks[], Block blocks[]);
 extern void repairBlocks(Block blocks[], Block backupBlocks[]);
-extern void rotate(Block blocks[], Block backupBlocks[]);
-extern void verticalDown(Block blocks[], Block backupBlocks[], sf::Text &score);
-extern void horizontalRight(Block blocks[], Block backupBlocks[]);
-extern void horizontalLeft(Block blocks[], Block backupBlocks[]);
-extern void fallDown(sf::Clock &clock, Block blocks[], Block backupBlocks[], sf::Text &score);
+extern void rotate(Block blocks[], Block backupBlocks[], bool firstPlayer);
+extern void verticalDown(Block blocks[], Block backupBlocks[], sf::Text &score, bool firstPlayer);
+extern void horizontalRight(Block blocks[], Block backupBlocks[], bool firstPlayer);
+extern void horizontalLeft(Block blocks[], Block backupBlocks[], bool firstPlayer);
+void fallDown(sf::Clock &clock, Block blocks[], Block backupBlocks[],  Block blocks2[], Block backupBlocks2[], sf::Text &score);
 #endif
--- include/tetrisLogic.hpp
+++ include/tetrisLogic.hpp
@@ -11,14 +11,14 @@ extern const int allBlockShapes[7][4] = {
 	2,3,4,5, // O
 };
 extern const int HEIGHT = 20, WIDTH = 10;
-int inactiveBlockSprites[HEIGHT][WIDTH] = { 0 };
+int inactiveBlockSprites[HEIGHT][WIDTH] = { 0 }, inactiveBlockSprites2[HEIGHT][WIDTH] = { 0 };
 int blockShape = rand() % 7, blockSprite = rand() % 8;
-int scoreValue = 0, offset = 0;
-double timer = 0.0, delay = 4.5;
-bool generateNewFigure = true;
+int scoreValue = 0, scoreValue2 = 0, offset = 0;
+double timer = 0.0, timer2 = 0.0, delay = 4.5;
+bool generateNewFigure = true, generateNewFigure2 = true, gameOver = false;
 
 sf::Texture tile;
 sf::Font font;
 sf::Sprite sprites[8];
-sf::Text scoreText("", font, 25), score("", font, 25);
+sf::Text scoreText("", font, 25), scoreText2("", font, 25),score("", font, 25), score2("", font, 25), gameOverText("", font, 80);
 #endif
